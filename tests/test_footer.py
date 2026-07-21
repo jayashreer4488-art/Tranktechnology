52c9e11182024ea4c47abd5e0ba689b19f7f1fe8
jayashreer4488-art <jayashree.r4488@gmail.com>
2026-07-21 21:17:49 +0530

    Update test_footer.py

--- tests/test_footer.py
+++ tests/test_footer.py
@@ -4,7 +4,7 @@ from pages.technologypage import technologypage
 from pages.footerpage import footerpage
 
 @pytest.mark.smoke
-@pytest.mark.order(4)
+# @pytest.mark.order(4)
 # @pytest.mark.skip
 def test_trad_footer_click(page):
    footer_tech= footerpage(page)
